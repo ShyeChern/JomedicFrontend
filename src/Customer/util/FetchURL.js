c922cb96b0446a8aa77c6f26af15e469d87febc0
Shye Chern Lim <46220892+ShyeChern@users.noreply.github.com>
2020-08-03 13:41:40 +0800

    Update FetchURL.js

--- src/Customer/util/FetchURL.js
+++ src/Customer/util/FetchURL.js
@@ -1,6 +1,6 @@
 // export const URL = "http://10.73.33.211:3000/query"; //Biocore 
 
-export const URL = "http://192.168.1.6:3000/customerquery"; //Home
-// export const URL = "http://157.245.148.221:3001/customerquery"; //cloud
+// export const URL = "http://192.168.1.7:3000/customerquery"; //Home
+export const URL = "http://157.245.148.221:3001/customerquery"; //cloud
 
 
