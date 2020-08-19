c12decc09594402e0c171ca9f9aff4d4c831244b
Shye Chern Lim <46220892+ShyeChern@users.noreply.github.com>
2020-08-19 20:27:47 +0800

    customer update
    integration test changes cycle2

--- src/Customer/screen/Appointment.js
+++ src/Customer/screen/Appointment.js
@@ -1,41 +1,9 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, View, SafeAreaView, SectionList, TouchableOpacity, RefreshControl } from 'react-native';
-import AntDesignIcon from 'react-native-vector-icons/AntDesign';
+import { Text, StyleSheet, View, TouchableOpacity, RefreshControl } from 'react-native';
+import { Agenda } from 'react-native-calendars';
 import { format, add } from 'date-fns';
 import { getCustomerId } from "../util/Auth";
 import { URL } from '../util/FetchURL';
-
-AppointmentItem = ({ appointment, that }) => {
-
-    return (
-        <View style={{ justifyContent: 'center', marginHorizontal: 10 }}>
-            {
-                appointment === 'No appointment booked' ?
-                    <View style={{ margin: 10, padding: 10, backgroundColor: '#F8F8F8' }}>
-                        <Text>No appointment booked</Text>
-                    </View>
-                    :
-                    <TouchableOpacity style={{ margin: 10, padding: 10, backgroundColor: '#F8F8F8' }}
-                        onPress={() => that.props.navigation.navigate('AppointmentDetail', {
-                            customerId: that.state.customerId,
-                            doctorId: appointment.doctorId,
-                            date: appointment.date,
-                            orderNo: appointment.orderNo,
-                            doctorName: appointment.doctorName,
-                            startTime: appointment.startTime,
-                            address: appointment.address,
-                            latitude: appointment.latitude,
-                            longtitude: appointment.longtitude
-                        })}>
-                        <Text>Doctor Name: {appointment.doctorName}</Text>
-                        <Text>Time: {appointment.startTime}</Text>
-                    </TouchableOpacity>
-
-            }
-
-        </View>
-    );
-}
 
 export default class Appointment extends Component {
 
@@ -44,11 +12,11 @@ export default class Appointment extends Component {
 
         this.state = {
             customerId: '',
-            appointmentList: [{
-                date: '',
-                data: []
-            }],
-            flatListLoading: true,
+            appointmentList: [],
+            isRefreshing: true,
+            items: {},
+            minDate: format(new Date(), 'yyyy-MM-dd'),
+            maxDate: format(add(new Date(), { days: 60 }), 'yyyy-MM-dd'),
         }
     }
 
@@ -56,15 +24,6 @@ export default class Appointment extends Component {
         await getCustomerId().then(response => {
             this.setState({ customerId: response });
         });
-        let appointment = [];
-        let currentDate = new Date();
-        for (let i = 0; i < 7; i++) {
-            let appointmentListItem = {};
-            appointmentListItem.date = format(add(currentDate, { days: i }), 'MMM dd (E)');
-            appointmentListItem.data = ['No appointment booked'];
-            appointment.push(appointmentListItem);
-        }
-        this.setState({ appointmentList: appointment })
 
         this.getAppointmentData();
     }
@@ -91,24 +50,13 @@ export default class Appointment extends Component {
                     .then((responseJson) => {
 
                         if (responseJson.result === true) {
-                            let appointment = [];
-                            let currentDate = new Date();
 
-                            // no appointment booked
-                            for (let i = 0; i < 7; i++) {
-                                let appointmentListItem = {};
-                                appointmentListItem.date = format(add(currentDate, { days: i }), 'MMM dd (E)');
-                                appointmentListItem.data = ['No appointment booked'];
-                                appointment.push(appointmentListItem);
-                            }
-                            responseJson.data.forEach(element => {
-                                let index = this.getIndexAppointment(appointment, element.date);
-                                appointment[index].data = element.appointment;
-                            });
                             this.setState({
-                                appointmentList: appointment,
-                                flatListLoading:false,
+                                appointmentList: responseJson.data,
+                                isRefreshing: false
                             });
+
+                            this.loadItems();
                         }
 
                     })
@@ -117,73 +65,122 @@ export default class Appointment extends Component {
                     });
             }
         }, 3000);
+
     }
 
     componentWillUnmount = () => {
         clearInterval(this.interval);
     }
 
-    getIndexAppointment = (array, date) => {
-        for (var i = 0; i < array.length; i++) {
-            if (array[i].date === date) {
-                return i;
+
+    loadItems(day) {
+
+        let newList = {};
+        for (let i = 0; i < 61; i++) {
+            const time = new Date(this.state.minDate).getTime() + i * 24 * 60 * 60 * 1000;
+            const date = new Date(time).toISOString().split('T')[0];
+
+            if (!newList[date]) {
+                newList[date] = [];
             }
         }
+
+
+        this.state.appointmentList.forEach(function (item, index) {
+            item.appointment.forEach(function (item, index) {
+                newList[item.date].push({
+                    appointment: item,
+                    doctorId: item.doctorId,
+                    date: item.date,
+                    orderNo: item.orderNo,
+                    doctorName: item.doctorName,
+                    startTime: item.startTime,
+                    address: item.address,
+                    latitude: item.latitude,
+                    longtitude: item.longtitude,
+                    doctorName: item.doctorName,
+                    startTime: item.startTime
+                })
+            })
+        })
+
+        this.setState({
+            items: newList
+        });
     }
 
     render() {
         return (
-            <View style={[styles.container]}>
-                <View style={[styles.title]}>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                        <Text style={[styles.textTitle]}>Today</Text>
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
-                        <Text style={[styles.textTitle]}>
-                            {
-                                format(new Date(), 'dd MMM yyyy')
-                            }
-                        </Text>
-                        <AntDesignIcon name='calendar' size={25} color='#FDAA26' />
-                    </View>
-                </View>
+            <View style={styles.container}>
+                <Agenda
+                    items={this.state.items}
+                    loadItemsForMonth={this.loadItems.bind(this)}
+                    selected={this.state.minDate}
+                    minDate={this.state.minDate}
+                    maxDate={this.state.maxDate}
+                    futureScrollRange={2}
+                    renderKnob={() => { return (<View ><Text style={{ fontStyle: 'italic', color: '#4A4A4A', paddingVertical: 3 }}>View Calendar</Text></View>); }}
+                    renderItem={(item) => {
+                        return (
+                            <TouchableOpacity style={styles.item}
+                                onPress={() => this.props.navigation.navigate('AppointmentDetail', {
+                                    customerId: this.state.customerId,
+                                    doctorId: item.doctorId,
+                                    date: item.date,
+                                    orderNo: item.orderNo,
+                                    doctorName: item.doctorName,
+                                    startTime: item.startTime,
+                                    address: item.address,
+                                    latitude: item.latitude,
+                                    longtitude: item.longtitude
+                                })}>
+                                <Text>Doctor Name: {item.doctorName}</Text>
+                                <Text>Time: {item.startTime}</Text>
+                            </TouchableOpacity>
+                        );
+                    }}
+                    renderEmptyDate={() => {
+                        return (
+                            <View style={styles.emptyDate}>
+                                <Text>No appointments booked</Text>
+                            </View>
+                        );
+                    }}
+                    rowHasChanged={(r1, r2) => { return r1.name !== r2.name; }}
+                    theme={{
+                        selectedDayBackgroundColor: '#FFD54E',
+                        selectedDotColor: '#FFFFFF',
+                        todayTextColor: '#FFD54E',
+                        dotColor: '#FDAA26',
+                        agendaTodayColor: '#FFD54E',
+                        agendaKnobColor: '#FFD54E',
+                    }}
+                    refreshControl={<RefreshControl refreshing={this.state.isRefreshing} />}
+                />
+            </View>
 
-                <View style={{ flex: 1 }}>
-                    <SafeAreaView style={styles.container}>
-                        <SectionList
-                            sections={this.state.appointmentList}
-                            refreshControl={<RefreshControl refreshing={this.state.flatListLoading} />}
-                            keyExtractor={(item, index) => item + index}
-                            renderItem={({ item }) => <AppointmentItem appointment={item} that={this} />}
-                            renderSectionHeader={({ section: { date } }) => (
-                                <View style={{ backgroundColor: '#ECECEC' }}>
-                                    <Text style={{ fontSize: 14, lineHeight: 19, marginHorizontal: 10 }}>{date}</Text>
-                                </View>
-                            )}
-                        />
-
-                    </SafeAreaView>
-                </View>
-
-            </View >
         )
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        justifyContent: 'flex-start',
         flex: 1,
-        backgroundColor: '#FFFFFF'
     },
-    title: {
-        marginVertical: 5,
-        flexDirection: 'row'
+    item: {
+        backgroundColor: '#FFFFFF',
+        flex: 1,
+        borderRadius: 5,
+        padding: 10,
+        marginRight: 10,
+        marginTop: 17,
+        height: 60,
+        justifyContent: 'center'
     },
-    textTitle: {
-        fontWeight: '600',
-        fontSize: 14,
-        lineHeight: 16,
-        color: '#FDAA26'
+    emptyDate: {
+        height: 15,
+        flex: 1,
+        paddingTop: 30,
+        justifyContent: 'center',
     }
 })
