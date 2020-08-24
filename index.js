3a054d802db4c773e8a57c4d689e494566fc0436
zhenyu550 <56509077+zhenyu550@users.noreply.github.com>
2020-08-24 09:44:54 +0800

    Provider New npm & Back button Bug Fix (24/08/2020)
    1. Added import 'react-native-gesture-handler' at index.js to fix not working Back button.
    2. New plugin (react-navigation-backhandler) is installed to handle hardware back button of Android. The install command:
    npm install react-navigation-backhandler

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
  * @format
  */
 
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 //import App from './src/eWallet/JOMEDIC WALLET/transferDetails';
 import App from './src/Customer/screen/index.js';
-// import App from './src/Provider/screen/Medication';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
